6c10ab23757ad5b33f66fc18fd5d8d834792f39b
likanug <likanug.g@qq.com>
2026-01-17 00:53:34 +0800

    feat: update the definition of AuthError, add Keylo's error code

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,7 @@
-use axum::Json;
 use axum::response::{IntoResponse, Response};
+use axum::Json;
 use http::StatusCode;
-use serde_json::json;
+use serde::Serialize;
 
 #[derive(Debug)]
 pub enum AuthError {
@@ -11,17 +11,25 @@ pub enum AuthError {
     InvalidToken,
 }
 
+#[derive(Debug, Serialize)]
+struct ErrorResponse {
+    code: u16,
+    message: String,
+}
+
 impl IntoResponse for AuthError {
     fn into_response(self) -> Response {
-        let (status, error_message) = match self {
-            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
-            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
-            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
-            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        let (status, code, message) = match self {
+            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, 1001, "Wrong credentials"),
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, 1002, "Missing credentials"),
+            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, 1003, "Token creation error"),
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, 1004, "Invalid token"),
         };
-        let body = Json(json!({
-            "error": error_message,
-        }));
+
+        let body = Json(ErrorResponse {
+            code,
+            message: message.to_string(),
+        });
         (status, body).into_response()
     }
 }
